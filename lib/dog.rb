b314e6a598f531cef23701b60ccd6179ba982458
andytouch93 <andy.touch.93@icloud.com>
2016-01-25 01:12:57 -0600

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+ 
+  def name
+    name = @name
+  end
+ 
+  def name=(name_string)
+    @name = (name_string)
+  end
+
+  def breed
+    breed = @breed
+  end
+ 
+  def breed=(breed_string)
+    @breed = (breed_string)
+  end
+ 
+end
---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+ 
+  def name
+    name = @name
+  end
+ 
+  def name=(name_string)
+    @name = (name_string)
+  end
+
+  def job
+    job = @job
+  end
+ 
+  def job=(job_string)
+    @job = (job_string)
+  end
+ 
+end
